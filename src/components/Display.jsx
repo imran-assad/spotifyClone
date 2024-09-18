63d277cbd2fc6165c55555f4b0c60d0fef518ab4
Imran Asad <imranasad5252@gmail.com>
2024-09-18 21:20:19 +0500

    background color added for display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import DisplayHome from "./DisplayHome";
-import { Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route, useLocation } from "react-router-dom";
 import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/frontend-assets/assets";
 
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  }, [isAlbum, bgColor]);
+
   return (
     <>
-      <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <div
+        ref={displayRef}
+        className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+      >
         <Routes>
           <Route path="/" element={<DisplayHome />}></Route>
           <Route path="/album/:id" element={<DisplayAlbum />}></Route>
