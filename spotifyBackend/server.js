a1c169aa0b89d2019ada67de38088a2c5d66ee33
Imran Asad <imranasad5252@gmail.com>
2024-09-21 19:28:32 +0500

    successfully created server

---
+++ spotifyBackend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+//initializing routes
+app.get("/", (req, res) => res.send("API working"));
+app.listen(port, () => console.log(`Server started on ${port}`));
