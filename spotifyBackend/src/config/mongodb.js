13212e25040637adb8dc2df3a402e41c2fe2183b
Imran Asad <imranasad5252@gmail.com>
2024-09-21 21:47:55 +0500

    configuration for cloudinary added

--- spotifyBackend/src/config/mongodb.js
+++ spotifyBackend/src/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Connection established");
   });
-  await mongoose.connect(`${process.env.MONDODB_URI}/spotify`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
 };
 
 export default connectDB;
